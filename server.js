993ef2c5dfb99302417313c978f2853a82b5ffae
srsydv <shrish@iiitmanipur.ac.in>
2022-08-31 12:25:11 +0530

    new update

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ require("./app/routes/dashboard.routes.js")(app);
 //     appId: `z6XNskF62yxOZh7Ege0yYG4MmlkqHRLAQAV2lHNH`
 // set port, listen for requests
 // const PORT = process.env.PORT || 3000;
-app.listen(3000, async() => {
+app.listen(process.env.PORT || 5000, async() => {
   console.log(`Server is running on 3000 port`);
   await Moralis.start({
     serverUrl: `https://hepeekqpfgaj.usemoralis.com:2053/server`,
